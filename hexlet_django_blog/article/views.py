a3599e1a4977263243562b5e04a778aa3f6ad14c
Sergei Ezerskii <gaftezer@gmail.com>
2023-10-23 11:48:33 +0300

    Make class-views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+
+
+class IndexView(View):
+    name = 'articles'
+
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'articles/index.html',
+            context={'name': self.name}
+        )
 
 
 def index(request):
